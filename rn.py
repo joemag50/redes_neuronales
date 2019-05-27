501be3939022b277ab55e5bfb6000563bc752c57
Jose Gamboa <j.gamboa@ukko.mx>
2019-05-26 23:05:08 -0500

    matplot lib shiet

--- rn.py
+++ rn.py
@@ -3,10 +3,11 @@
 # Python 2.7.12
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from matplotlib import pyplot as plt 
 
 if __name__ == '__main__':
-	read_y = np.loadtxt('test.txt', delimiter=',', usecols=(1, ))
 	read_x = np.loadtxt('test.txt', delimiter=',', usecols=range(1))
+	read_y = np.loadtxt('test.txt', delimiter=',', usecols=(1, ))
 	x = np.array(read_x).reshape((-1, 1))
 	y = np.array(read_y)
 	print(x)
@@ -25,7 +26,14 @@ if __name__ == '__main__':
 	print('predicted response:', y_pred)
 	y_pred = model.intercept_ + model.coef_ * x
 	print('predicted response:', y_pred)
-	x_new = np.arange(5).reshape((-1, 1))
+	x_new = np.arange(2000).reshape((-1, 1))
 	print(x_new)
 	y_new = model.predict(x_new)
 	print(y_new)
+
+	plt.title("BTC") 
+	plt.xlabel("Time") 
+	plt.ylabel("Price") 
+	plt.plot(read_x,read_y, label='Original') 
+	plt.plot(x,y_pred, label='Predicted') 
+	plt.show()
